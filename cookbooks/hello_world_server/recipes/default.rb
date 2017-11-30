25a229ce9924b3326c0f1826dc8fea4b2f48ff2c
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-11-30 10:53:20 -0500

    changed recipe

--- cookbooks/hello_world_server/recipes/default.rb
+++ cookbooks/hello_world_server/recipes/default.rb
@@ -1,48 +1,52 @@
-#
-# Cookbook:: hello_world_server
+# Cookbook Name:: setup_hello_world
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright (c) 2015 The Authors, All Rights Reserved.
 
-#install tomcat
+
+
+# install tomcat
 yum_package 'tomcat' do
-	options "-y"
+    options "-y"
 end
+
 service 'tomcat' do
-	action [:enable, :start]
-	#provider Chef::Provider::Service::Systemd
+  action [:enable, :start]
+  #provider Chef::Provider::Service::Systemd
 end
-#finding docker image that images init find for platform in kitchen yml
-# sol 2:
+# finding docker image that images init find for platform in kitchen yml
+# sol 2: 
 
-#yum install wget
+
+# yum install wget
 yum_package 'wget' do
-	options "-y"
+    options "-y"
 end
 
-template '/etc/yum.repos.d/Nexus-Hello_World.repo' do
-	source 'Nexus-Hello_World.repo'
-	owner 'centos'
-	group 'centos'
-	mode '0755'
+template '/etc/yum.repos.d/Nexus-Hello-World.repo' do
+  source 'Nexus-Hello-World.repo'
+  owner 'centos'
+  group 'centos'
+  mode '0755'
 end
 
 bash 'setup_yum' do
-	cwd ::File.dirname('/home/centos')
-	code <<-EOH
-		yum clean all
-		yum install hello-world -y
-		EOH
-	end
+  cwd ::File.dirname('/home/centos')
+  code <<-EOH
+    yum clean all
+    yum install hello-world -y
+    EOH
+end
 
-#rpm_package 'hello-world-sample-random-gen-1.0.2-rpm' do
-# action :install
-# source '/tmp/hello-world-sample-random-gen-1.0.2-rpm.rpm'
-# end
+
+# rpm_package 'hello-world-sample-random-gen-1.0.2-rpm' do
+#   action :install
+#   source '/tmp/hello-world-sample-random-gen-1.0.2-rpm.rpm'
+# end	
 
 bash 'restart_tomcat' do
-	cwd ::File.dirname('/tmp')
-	code <<-EOH
-		service tomcat restart
-		EOH
-	end
+  cwd ::File.dirname('/tmp')
+  code <<-EOH
+    service tomcat restart
+    EOH
+end
