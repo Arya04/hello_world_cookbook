c273cf224936783476bdd21e93102ed8d3a42d61
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-12-01 10:07:41 -0500

    edited knife config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,6 +5,6 @@ log_level                :info
 log_location             STDOUT
 node_name                "chefadmin"
 client_key               "#{current_dir}/chefadmin.pem"
-chef_server_url          "https://172.31.2.242/organizations/4thcoffee"
+chef_server_url          "http://172.31.2.242/organizations/4thcoffee"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 knife[:aws_credential_file]="/home/centos/aws.txt"
